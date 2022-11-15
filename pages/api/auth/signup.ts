2caee313ca5d6176c45c1426b2a1080eac4de92f
bhanuprakash8989 <bhanuprakash.ravipati@soulpageit.com>
2022-11-15 16:11:34 +0530

    init

---
+++ pages/api/auth/signup.ts
@@ -0,0 +1,50 @@
+import nextConnect from "next-connect";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import db from "lib/db.server";
+
+const handler = nextConnect();
+
+export default handler.post(async (req: any, res: any) => {
+  try {
+    const { firstName, lastName, phoneNumber, email } = req.body;
+    let { password } = req.body;
+
+    if (!firstName || !lastName || !phoneNumber || !email || !password)
+      res.status(400).json("Enter valid details");
+
+    const isExists: any = await db.user.findUnique({
+      where: { email },
+    });
+
+    if (!isExists) {
+      const saltRounds = 10;
+      bcrypt.genSalt(saltRounds, function (err, salt) {
+        bcrypt.hash(password, salt, async function (err, hash) {
+          try {
+            req.body.password = hash;
+            // create new user
+            const user = await db.user.create({ data: req.body });
+            const token = jwt.sign({ id: user.id }, "soulpage", {
+              expiresIn: "3d",
+            });
+
+            res
+              .status(201)
+              .json({
+                id: user.id,
+                token,
+                user,
+                message: "Successfully Signed up",
+              });
+          } catch (error: any) {
+            res.status(400).json({ message: "Error Occured" });
+          }
+        });
+      });
+    } else res.status(400).json({ message: "Email address already taken" });
+  } catch (e: any) {
+    console.log(e);
+    res.status(400).json({ message: e });
+  }
+});
