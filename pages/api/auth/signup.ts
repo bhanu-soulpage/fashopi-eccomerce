167a2780bd3e162bba0bea6b08a4c873962bfbb7
bhanuprakash8989 <bhanuprakash.ravipati@soulpageit.com>
2022-11-16 12:03:45 +0530

    errors resolved init

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -13,30 +13,29 @@ export default handler.post(async (req: any, res: any) => {
     if (!firstName || !lastName || !phoneNumber || !email || !password)
       res.status(400).json("Enter valid details");
 
-    const isExists: any = await db.user.findUnique({
+    const user: any = await db.user.findUnique({
       where: { email },
     });
+    console.log(user);
 
-    if (!isExists) {
+    if (!user) {
       const saltRounds = 10;
       bcrypt.genSalt(saltRounds, function (err, salt) {
         bcrypt.hash(password, salt, async function (err, hash) {
           try {
-            req.body.password = hash;
+            const password_hash = hash;
+            req.body.password = password_hash;
             // create new user
             const user = await db.user.create({ data: req.body });
-            const token = jwt.sign({ id: user.id }, "soulpage", {
+            const token = jwt.sign({ id: user.id }, "fashopiy", {
               expiresIn: "3d",
             });
-
-            res
-              .status(201)
-              .json({
-                id: user.id,
-                token,
-                user,
-                message: "Successfully Signed up",
-              });
+            res.status(201).json({
+              id: user.id,
+              token,
+              user,
+              message: "Successfully Signed up",
+            });
           } catch (error: any) {
             res.status(400).json({ message: "Error Occured" });
           }
